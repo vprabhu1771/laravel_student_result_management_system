344ed18405615e9058ea1fb41db94da85cd18411
vprabhu1771 <vprabhu1771@gmail.com>
2024-12-26 18:23:42 +0530

    groups that belong to the section relationship added

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -17,4 +17,12 @@ class Section extends Model
     protected $fillable = [
         'name'
     ];
+
+    /**
+     * The groups that belong to the section.
+     */
+    public function groups()
+    {
+        return $this->belongsToMany(Group::class, 'group_section', 'section_id', 'group_id');
+    }
 }
