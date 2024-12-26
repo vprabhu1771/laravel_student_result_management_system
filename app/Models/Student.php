8a5bedaad23533584e8183b602ba486190c9b542
vprabhu1771 <vprabhu1771@gmail.com>
2024-12-26 18:32:45 +0530

    group sections that the student relationship added

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -21,4 +21,13 @@ class Student extends Model
         'gender',
         'dob'
     ];
+
+    /**
+     * The group sections that the student belongs to.
+     */
+    public function groupSections()
+    {
+        return $this->belongsToMany(GroupSection::class, 'student_group_section', 'student_id', 'group_section_id')
+                    ->withTimestamps();
+    }
 }
