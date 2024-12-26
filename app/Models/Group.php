7f303830fdd88728926174c5ef0b33decac48f95
vprabhu1771 <vprabhu1771@gmail.com>
2024-12-26 18:35:38 +0530

    group sections associated with the group relationship added

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -26,4 +26,12 @@ class Group extends Model
     {
         return $this->belongsToMany(Section::class, 'group_section', 'group_id', 'section_id');
     }
+
+    /**
+     * The group sections associated with the group.
+     */
+    public function groupSections()
+    {
+        return $this->hasMany(GroupSection::class);
+    }
 }
